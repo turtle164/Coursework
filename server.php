1be314f27bb0890d9778f3fdda8c1ad7bffedd51
turtle164 <anamolcrest47@gmail.com>
2023-08-10 19:11:26 +0545

    CourseWork

--- server.php
+++ server.php
@@ -1,84 +1,43 @@
 <?php
-session_start();
+// Database configuration
+$servername = "localhost";
+$username = "root"; // Default username for XAMPP
+$password = "";     // Default password for XAMPP
+$dbname = "cw_1"; // Replace with your actual database name
 
-// initializing variables
-$username = "";
-$email    = "";
-$errors = array(); 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// connect to the database
-$db = mysqli_connect('localhost', 'root', '', 'cw_1');
-
-// REGISTER USER
-if (isset($_POST['reg_user'])) {
-  // receive all input values from the form
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
-  if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
-  }
-
-  // first check the database to make sure 
-  // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-  $result = mysqli_query($db, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
-  
-  if ($user) { // if user exists
-    if ($user['username'] === $username) {
-      array_push($errors, "Username already exists");
-    }
-
-    if ($user['email'] === $email) {
-      array_push($errors, "email already exists");
-    }
-  }
-
-  // Finally, register user if there are no errors in the form
-  if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
-
-  	$query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
-  	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
-  }
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+// After form validation
+$newUsername = $_POST['newUsername'];
+$newPassword = $_POST['newPassword'];
 
-// LOGIN USER
-if (isset($_POST['login_user'])) {
-  $username = mysqli_real_escape_string($db, $_POST['username']);
-  $password = mysqli_real_escape_string($db, $_POST['password']);
+$hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
 
-  if (empty($username)) {
-  	array_push($errors, "Username is required");
-  }
-  if (empty($password)) {
-  	array_push($errors, "Password is required");
-  }
+$sql = "INSERT INTO users (username, password) VALUES ('$newUsername', '$hashedPassword')";
+$conn->query($sql);
 
-  if (count($errors) == 0) {
-  	$password = md5($password);
-  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-  	$results = mysqli_query($db, $query);
-  	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: index.php');
-  	}else {
-  		array_push($errors, "Wrong username/password combination");
-  	}
-  }
+// After form submission
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT id, password FROM users WHERE username='$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+    if (password_verify($password, $row['password'])) {
+        // User is authenticated
+    } else {
+        // Invalid password
+    }
+} else {
+    // User not found
 }
+$conn->close();
 
 ?>
